403942862dc98385f22ed7a908859f3d45eeced4
Derick <fezcolaurent@gmail.com>
2025-11-01 18:01:48 +0300

    test

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine" , "ejs")
+
 app.get("/", (req, res) => {
-  res.send("hello from my get route")
+    res.send("hello from my get route test")
+    
 })
 
 
