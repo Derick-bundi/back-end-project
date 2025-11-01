5d175242582082f132619a4904605d008985d9d4
Derick <fezcolaurent@gmail.com>
2025-11-01 18:58:25 +0300

    aded two routes

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const app = express()
 
-app.set("view engine" , "ejs")
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.send("hello from my get route test")
-    
+    // res.send("hello from my get route test")
+    res.render("homepage")
 })
 
 
 
-app.listen(3000)
+app.get("/login", (req,res) => { 
+
+res.render("login")
+})
+
+
+app.listen(3000, ()=> {
+    console.log('app is listening on port 3000')
+})
